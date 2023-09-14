62665df2f0d6354357a84b4f3e3991a0c7cc82e8
Michal Sporek <msporek@gmail.com>
2023-09-14 13:08:25 +0200

    Introduced a documentary comment for interface IAlgoRule.cs.

--- CoinmarketcapTracker.Analytics/IAlgoRule.cs
+++ CoinmarketcapTracker.Analytics/IAlgoRule.cs
@@ -2,6 +2,10 @@
 
 namespace CoinmarketcapTracker.Analytics;
 
+/// <summary>
+/// Interface meant to be implemented by classes the provide the algorithmic rules functionality, 
+/// running algorithmic rules against data and returning a result that indicates the amount of profit / loss. 
+/// </summary>
 public interface IAlgoRule
 {
     public double Run(Dictionary<DateOnly, List<Currency>> dateToRanking);
